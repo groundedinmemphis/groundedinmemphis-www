273b3fe8d7ff6657f56312076dc794953626f5dc
Matt Daniels <matthew.f.daniels@gmail.com>
2019-03-10 15:18:02 -0400

    here

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 import GroundedLogo from 'assets/svg/grounded-workmark-no-mark.svg';
 import GroundedLogoWithMark from 'assets/svg/grounded-workmark.svg';
 import IconMenu from 'assets/svg/icon-menu.svg';
+import XMenu from 'assets/svg/x.svg';
 
 import s from './Header.scss';
 import { Link as HeaderLink } from 'components/header/Link';
@@ -13,7 +14,6 @@ interface IHeaderProps {
 }
 
 export const Header = (props: IHeaderProps) => {
-
   const { children, theme } = props;
   const [isOpen, setIsOpen] = React.useState(false);
   const header__light = theme === 'light'; // tslint:disable-line
@@ -36,6 +36,9 @@ export const Header = (props: IHeaderProps) => {
           <div
             className={s('header__navigation', { header__navigation__open })}
           >
+            <button className={s.closing_mobile_menu} onClick={handleClick}>
+              <XMenu />
+            </button>
             <div className={s.header__left}>
               <Link to="/" className={s.header__logo}>
                 <GroundedLogo className={s.header__logoSvg} />
@@ -46,7 +49,7 @@ export const Header = (props: IHeaderProps) => {
               <HeaderLink name="Meet the Team" to="/meet-the-team" />
               <HeaderLink name="News" to="/news" />
             </div>
-            <div className={s.header__right}>
+            <div className={s.header__right} onClick={handleClick}>
               <HeaderLink name="Partner with Us" to="/#partner" />
               <HeaderLink name="Donate" to="/donate" />
             </div>
