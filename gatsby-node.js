29bbb158ed3e8305b55756be1d6916c0456cf7e0
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-03-09 19:00:45 -0500

    generate project pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  return new Promise(resolve => {
+    graphql(`
+      query {
+        allPrismicProject {
+          edges {
+            node {
+              uid
+              prismicId
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allPrismicProject.edges.forEach(({ node }) => {
+        actions.createPage({
+          path: `project/${node.uid}`,
+          component: path.resolve('./src/templates/Project.tsx'),
+          context: node,
+        });
+      });
+      resolve();
+    });
+  });
+};
