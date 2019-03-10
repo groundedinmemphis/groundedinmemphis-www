450a657a0bcc5b1e9fa137a37ca819809eb66e3c
Matt Daniels <matthew.f.daniels@gmail.com>
2019-03-10 19:11:37 -0400

    donate

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import { Container } from 'components/container/Container';
+import s from './donate.scss';
 
 import { BlockText } from 'components/block-text/BlockText';
 import { ItemList } from 'components/item-list/ItemList';
@@ -9,12 +10,36 @@ import { PageIntro } from 'components/intro/PageIntro';
 
 export default () => {
   return (
-    <>
+    <div className={s.donate__page}>
       <Helmet title="Purpose" />
 
-      <p>Support us with a donation.</p>
-      <p>When you donate, you'll do more than lorem</p>
       <script src="https://donorbox.org/widget.js" paypalExpress="false" />
+
+      <Container>
+        <PageIntro
+          title={
+            <>
+              Support us <br />
+              with a donation
+            </>
+          }
+        />
+      </Container>
+      <div className={s.block__text}>
+        <BlockText
+          heading=""
+          description={
+            <>
+              <p>
+                When you donate to Grounded, you'll be helping to support our
+                collaborators, our projects and programs, and communities in
+                Memphis.
+              </p>
+            </>
+          }
+        />
+      </div>
+
       <iframe
         src="https://donorbox.org/embed/hello-3"
         height="685px"
@@ -23,6 +48,8 @@ export default () => {
           maxWidth: '500px',
           minWidth: '310px',
           maxHeight: 'none!important',
+          margin: '0 auto',
+          display: 'block',
         }}
         seamless="seamless"
         name="donorbox"
@@ -30,71 +57,6 @@ export default () => {
         scrolling="no"
         allowpaymentrequest
       />
-
-      <Container>
-        <PageIntro
-          title={
-            <>
-              What makes us <br />
-              grounded
-            </>
-          }
-        />
-      </Container>
-
-      <BlockText
-        heading="About Grounded"
-        description={
-          <>
-            <p>
-              Founded in 2012, Grounded is a nonprofit artistic collective
-              rooted in the Memphis community. We believe artists have the power
-              to inspire students, communities, and cities to unite, pursue
-              their passions, and end cycles of violence.
-            </p>
-
-            <p>
-              Our core team and expansive network of collaborators bring an
-              exceptional level of talent, professionalism and authenticity to
-              all of our projects.
-            </p>
-          </>
-        }
-      />
-
-      <BlockText
-        heading="What We Do"
-        description={
-          <>
-            <p>
-              Our shared purpose is to create content that is meaningful and
-              inspiring while serving the larger mission of using creativity as
-              a means of connecting and activating communities around important
-              social issues.
-            </p>
-
-            <p>
-              We are guided by <a href="#">our methodology</a> of using art to
-              advance non-violence education and as a tool for change, growth,
-              and transformation. We know there is greater opportunity for art
-              and mindfulness paired with a network of collaborators to create
-              safer cities and a healthier community.
-            </p>
-          </>
-        }
-      />
-
-      <BlockText
-        heading="Our Partners"
-        description={
-          <>
-            <p>
-              We work with the best lorem ipsum dolor sit amet del donde
-              seninkininde.
-            </p>
-          </>
-        }
-      />
-    </>
+    </div>
   );
 };
